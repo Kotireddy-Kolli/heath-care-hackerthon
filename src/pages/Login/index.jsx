d662e45a9ea1de9fecad0dd6660253f074b88a82
MAHAVIR GADIPUDI (CRGL-THIRDPARTY.COM) <mahavir_gadipudi@crgl-thirdparty.com>
2025-11-08 17:37:55 +0530

    validations

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -39,14 +39,20 @@ export default function Login() {
       >
         <TextField
           label="Email"
-          {...register('email', { required: 'Email is required' })}
+          {...register('email', { required: 'Email is required',   pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: "Enter a valid email",
+          }, })}
           error={!!errors.email}
           helperText={errors.email?.message}
         />
         <TextField
           label="Password"
           type="password"
-          {...register('password', { required: 'Password is required' })}
+          {...register('password', { required: 'Password is required' , minLength: {
+            value: 6,
+            message: "Password must be at least 6 characters",
+          },})}
           error={!!errors.password}
           helperText={errors.password?.message}
         />
